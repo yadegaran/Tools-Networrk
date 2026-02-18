cd948fdf5c25e22947547c14c255df888ca5fa78
yadegaran <124578235+yadegaran@users.noreply.github.com>
2026-02-19 01:23:30 +0330

    -fix show version v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 6
-        versionName = "5.1"
+        versionName = "6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
